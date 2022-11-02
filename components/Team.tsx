5631b66698708dd176dfb014cac544afa2f9f7dd
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-11-02 12:28:18 -0300

    Update styles page teams

--- components/Team.tsx
+++ components/Team.tsx
@@ -3,12 +3,12 @@ import styles from '../styles/components/Team.module.css'
 interface TeamProps {
     teamName: string
     teamParticipants: string[]
+    index: number
 }
 
-export function Team({ teamName, teamParticipants }: TeamProps) {
-
+export function Team({ teamName, teamParticipants, index }: TeamProps) {
     return (
-        <div>
+        <div className={`${styles.team} ${index % 2 == 0 ? styles.teamWhite : styles.teamBlack}`} >
             {teamName} <br></br>
             {teamParticipants}
         </div>
--- pages/Teams.tsx
+++ pages/Teams.tsx
@@ -1,21 +1,30 @@
 import { Navbar } from "../components/Navbar";
 import { Team } from "../components/Team"
+import styles from "../styles/pages/Teams.module.css"
 
 export default function Teams() {
   return (
     <div>
       <Navbar fromParentSelected={6} />
 
-      <div>
-        EQUIPES CONFIRMADAS
+      <div className={styles.teamsHeader}>
+        <p>---------- EQUIPES CONFIRMADAS ----------</p>
       </div>
 
-      <div>
-        <Team key={0} teamName="Manjava" teamParticipants={["luiz", "matheus"]} />
-        <Team key={1} teamName="Pythaun" teamParticipants={["joao", "avonso"]} />
-        <Team key={2} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
+      <div className={styles.teamsContainer}>
+        <Team key={0} index={0} teamName="Manjava" teamParticipants={["luiz", "matheus"]} />
+        <Team key={1} index={1} teamName="Pythaun" teamParticipants={["joao", "avonso"]} />
+        <Team key={2} index={2} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
+        <Team key={2} index={3} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
+        <Team key={2} index={4} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
+        <Team key={2} index={5} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
+        <Team key={2} index={6} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
+        <Team key={2} index={7} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
+        <Team key={2} index={8} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
+        <Team key={2} index={9} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
+        <Team key={2} index={10} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
+        <Team key={2} index={11} teamName="Malloc sem free" teamParticipants={["lara", "kevin"]} />
       </div>
-
     </div>
   )
 }
