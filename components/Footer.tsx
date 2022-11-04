c043124bd5ca8e4079eb3ef1256e19ac248d730d
fiorotticaio <caiofiorotti@gmail.com>
2022-11-04 19:55:24 -0300

    :lipstick: fotter component done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
+import { BsTelephone, BsInstagram } from 'react-icons/bs'
+import { HiOutlineMailOpen } from 'react-icons/hi';
 
 import styles from '../styles/components/Footer.module.css';
 import logoPET from '../assets/logoPET.png';
 import logoUfes from '../assets/logoUfes.png';
 import logoLAR from '../assets/logoLAR.png';
-import topquinho from '../assets/topquinho.png';
+import topquinho from '../assets/baloon.png';
 
 interface FooterProps {
 
@@ -22,30 +24,44 @@ export function Footer({}:FooterProps){
         <div className={styles.organization}>
             <h2>REALIZAÇÃO</h2>
             <div className={styles.logos}>
-                <Image className={styles.logo} src={logoPET} alt="Logo do PET Eng Comp" />
-                <Image className={styles.logo} src={logoUfes} alt="Logo da ufes" />
-                <Image className={styles.logo} src={logoLAR} alt="Logo do LAR" />
+                <div className={styles.organizationImage}>
+                  <Image className={styles.logo} src={logoPET} alt="Logo do PET Eng Comp" />
+                </div>
+                <div className={styles.organizationImage}>
+                  <Image className={styles.logo} src={logoUfes} alt="Logo da ufes" />
+                </div>
+                <div className={styles.organizationImage}>
+                  <Image className={styles.logo} src={logoLAR} alt="Logo do LAR" />
+                </div>
             </div>
         </div>
 
 
         <footer className={styles.bottomFooter}>
 
-          <Image className={styles.topqui} src={topquinho} alt="Mascote do Topcom 20 gigante" />
+          <div className={styles.topqui}>
+            <Image className={styles.top} src={topquinho} alt="Mascote do Topcom 20 gigante" />
+          </div>
             
           <div className={styles.anotations}>
             <div className={styles.contact}>
               <div>
                 <h3>TELEFONES</h3>
-                {/* phone incon */}
-                <p>(27) 3335-2139</p>
-                {/* phone incon */}
-                <p>(27) 4009-2139</p>
+                <div className={styles.contactInfo}>
+                  <BsTelephone />
+                  <p>(27) 3335-2139</p>
+                </div>
+                <div className={styles.contactInfo}>
+                  <BsTelephone />
+                  <p>(27) 4009-2139</p>
+                </div>
               </div>
               <div>
                 <h3>FALE CONOSCO</h3>
-                {/* mail incon */}
-                {/* inst icon */}
+                <div className={styles.contactIcons}>
+                  <HiOutlineMailOpen size={70} />
+                  <BsInstagram  size={70} />
+                </div>
               </div>
             </div>
             <p>Created by PET Engenharia da Computação - 2023 - All rights reserved</p>
