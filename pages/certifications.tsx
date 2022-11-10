603cedf774588ca440a7668583d9df7c5ab5698f
João Victor Morais <jv.moraiscb@gmail.com>
2022-11-09 21:45:27 -0300

    :tada: start certifications page

--- pages/certifications.tsx
+++ pages/certifications.tsx
@@ -1,10 +1,15 @@
-import { Navbar } from "../components/Navbar";
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import ImageBanner from '../components/ImageBanner'
+import { Navbar } from '../components/Navbar'
 
-export default function Certifications(){
-  return (
-    <div>
-      <Navbar fromParentSelected={3} />
-      topcom legal
-    </div>
-  )
+export default function Certifications() {
+    return (
+        <div>
+            <Navbar fromParentSelected={3} />
+            <Header text="CERTIFICADOS"></Header>
+            <ImageBanner variant={2}></ImageBanner>
+            <Footer></Footer>
+        </div>
+    )
 }
