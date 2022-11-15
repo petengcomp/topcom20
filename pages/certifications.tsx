03c846e4c166b230569c98f0e0f7e07674c69021
jvmoraiscb <jv.moraiscb@gmail.com>
2022-11-15 20:43:48 -0300

    :white_check_mark: fix certifications page align

--- pages/certifications.tsx
+++ pages/certifications.tsx
@@ -1,31 +1,31 @@
-import { Button } from '../components/Button'
-import { Footer } from '../components/Footer'
-import { Header } from '../components/Header'
-import ImageBanner from '../components/ImageBanner'
-import { Navbar } from '../components/Navbar'
-import { InputButton } from '../components/InputButton'
-import styles from '../styles/pages/Certifications.module.css'
-import Image from 'next/image'
-import bodyImage1 from '../assets/bodyImage1.png'
+import { Button } from "../components/Button"
+import { Footer } from "../components/Footer"
+import { Header } from "../components/Header"
+import ImageBanner from "../components/ImageBanner"
+import { Navbar } from "../components/Navbar"
+import { InputButton } from "../components/InputButton"
+import styles from "../styles/pages/Certifications.module.css"
+import Image from "next/image"
+import bodyImage1 from "../assets/bodyImage1.png"
 
 export default function Certifications() {
-    return (
-        <div>
-            <Navbar fromParentSelected={3} />
-            <Image
-                className={styles.bodyImageContainer}
-                src={bodyImage1}
-                alt="Imagem de corpo do site"
-            />
-            <Header text="CERTIFICADOS"></Header>
-            <body className={styles.body}>
-                <InputButton
-                    textInput="Email"
-                    textButton="SOLICITAR CERTIFICADOS"
-                ></InputButton>
-            </body>
-            <ImageBanner variant={2}></ImageBanner>
-            <Footer></Footer>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar fromParentSelected={3} />
+      <Image
+        className={styles.bodyImageContainer}
+        src={bodyImage1}
+        alt="Imagem de corpo do site"
+      />
+      <Header text="CERTIFICADOS"></Header>
+      <div className={styles.body}>
+        <InputButton
+          textInput="Email"
+          textButton="SOLICITAR CERTIFICADOS"
+        ></InputButton>
+      </div>
+      <ImageBanner variant={2}></ImageBanner>
+      <Footer></Footer>
+    </div>
+  )
 }
