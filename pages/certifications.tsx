1ce57547071e6dfd746a4e70499362882061d1f7
Matheus <mt.schreiber01@gmail.com>
2022-11-18 20:24:20 -0300

    :wrench: refactor button component

--- pages/certifications.tsx
+++ pages/certifications.tsx
@@ -1,14 +1,23 @@
-import { Button } from "../components/Button"
+import styles from "../styles/pages/Certifications.module.css"
+import bodyImage1 from "../assets/bodyImage1.png"
+import { useState } from "react"
+import Image from "next/image"
+
 import { Footer } from "../components/Footer"
 import { Header } from "../components/Header"
-import ImageBanner from "../components/ImageBanner"
 import { Navbar } from "../components/Navbar"
-import { InputButton } from "../components/InputButton"
-import styles from "../styles/pages/Certifications.module.css"
-import Image from "next/image"
-import bodyImage1 from "../assets/bodyImage1.png"
+import ImageBanner from "../components/ImageBanner"
+import { Button } from "../components/Button"
 
 export default function Certifications() {
+  const [ email, setEmail ] = useState<string>();
+
+  async function requestCertificate(e:any) {
+    e.preventDefault()
+    alert(email)
+  }
+  
+  
   return (
     <div>
       <Navbar fromParentSelected={3} />
@@ -17,15 +26,27 @@ export default function Certifications() {
         src={bodyImage1}
         alt="Imagem de corpo do site"
       />
-      <Header text="CERTIFICADOS"></Header>
-      <div className={styles.body}>
-        <InputButton
-          textInput="Email"
-          textButton="SOLICITAR CERTIFICADOS"
-        ></InputButton>
+      <Header text="CERTIFICADOS" />
+
+      <div className={styles.formsContainer}>
+        <form onSubmit={requestCertificate}>
+          <label htmlFor="email"></label>
+          <input
+            className={styles.inputBox}
+            onChange={(e)=>setEmail(e.target.value)}
+            type="email"
+            value={email}
+            placeholder="EMAIL"
+            required
+          />
+          <Button text="SOLICITAR CERTIFICADO" />
+
+
+        </form>
       </div>
-      <ImageBanner variant={2}></ImageBanner>
-      <Footer></Footer>
+
+      <ImageBanner variant={2} />
+      <Footer />
     </div>
   )
 }
