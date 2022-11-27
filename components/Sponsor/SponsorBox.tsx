224541d8255048b49ae3697bcc692ff2b74faf68
fiorotticaio <caiofiorotti@gmail.com>
2022-11-27 12:59:05 -0300

    :sparkles: crating sponsors page

---
+++ components/Sponsor/SponsorBox.tsx
@@ -0,0 +1,25 @@
+import styles from '../../styles/components/Sponsor/SponsorBox.module.css'
+
+interface SponsorBoxProps {
+    imagePath: string
+    text: string
+    textColor: string
+}
+
+export function SponsorBox({imagePath, text, textColor}: SponsorBoxProps) {
+
+    const parameterStyles = {
+        p: {
+            color: {textColor}
+        }
+    }
+
+    return (
+        <div style={parameterStyles.p} className={styles.main_container}>
+            <div className={styles.image_container}>
+                <img src={imagePath} alt="Logo do Patrocinador"/>
+            </div>
+            <p>{text}</p>
+        </div>
+    )
+} 
