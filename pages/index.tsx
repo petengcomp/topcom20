06ab9a505f120062598bf33550f06d06e1086b7d
Matheus <mt.schreiber01@gmail.com>
2023-05-15 21:06:48 -0300

    :bug: fix boca de treinamento link

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,7 +71,7 @@ export default function Home() {
             <br/><br/>
             O sistema é usado para que se faça o uso de submissão e correção automatica com finalidadede verificar e validar a consistência de um algoritmo através de vários casos teste.
             <br/><br/>
-            Faça seu cadastro e treine para a competição. Para mais informações, <a href="http://boca.pet.inf.ufes.br/boca/" target="_blank">clique aqui.</a></p>}
+            Faça seu cadastro e treine para a competição. Para mais informações, <a href="http://boca.pet.inf.ufes.br/" target="_blank">clique aqui.</a></p>}
           icon={<FiCoffee/>}
         />
 
